2f6153966c6273769afa974ec29400a14494d832
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-01-13 11:52:11 +0600

    Fix formatting and add missing newline at end of file

---
+++ Create A Class Book/Create A Class Book.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Book{
+    public:
+    string title;
+    string author;
+    int pubyr;
+    public:
+    void display_details(){
+        cout<<"Title: "<<title<<endl<<"Author: "<<author<<endl<<"Publication Year: "<<pubyr<<endl;
+    }
+    
+    
+};
+
+int main() {
+	Book OOP;
+	OOP.title="Object-Oriented Programming in C++";
+	OOP.author="Robert Lafore";
+	OOP.pubyr=2002;
+	OOP.display_details();
+	return 0;
+
+}
