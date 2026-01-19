480714a086c1f134226730af846a92e7409fc5c4
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-01-19 14:03:46 +0600

    Update My Car Class-Object.cpp

--- Lab 01/Problem 02: My Car Class/My Car Class-Object.cpp
+++ Lab 01/Problem 02: My Car Class/My Car Class-Object.cpp
@@ -1,34 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-class animal{
-    public:
+
+class animal {
+public:
     string color;
     string sound;
     string name;
-    void printinfo(){
-        cout<<"Color: "<<color<<endl<<"Sound: "<<sound<<endl;
+
+    void printinfo() {
+        cout << "Color: " << color << endl;
+        cout << "Sound: " << sound << endl;
     }
-    void showname(){
-         cout<<"Name: "<<name<<endl;
+
+    void showname() {
+        cout << "Name: " << name << endl;
     }
 };
 
 int main() {
-    	animal Cat;
-    	Cat.color= "White";
-    	Cat.sound= "Meow";
-    	Cat.name="Zebu";
-    	Cat.printinfo();
-    	Cat.showname();
-    	
-    	cout<<endl;
-    	
-    	animal Bear;
-    	Bear.color= "White";
-    	Bear.sound= "Garrr";
-    	Bear.name="Zabu";
-    	Bear.printinfo();
-    	Bear.showname();
-    return 0;
+    animal Cat;
+    Cat.color = "White";
+    Cat.sound = "Meow";
+    Cat.name  = "Zebu";
+    Cat.printinfo();
+    Cat.showname();
 
+    cout << endl;
+
+    animal Bear;
+    Bear.color = "White";
+    Bear.sound = "Garrr";
+    Bear.name  = "Zabu";
+    Bear.printinfo();
+    Bear.showname();
+
+    return 0;
 }
