691a33d8eaff3c6152e81c427d8f577c72536997
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-01-27 13:59:43 +0600

    Create P.P.cpp

---
+++ Lab 04/P.P.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class myclass{
+        int a;
+        public:
+        myclass(int x);
+        int get();
+    
+    };
+	myclass::myclass(int x){
+	    a=x;
+	}
+	int myclass::get(){
+	    return a;
+	}
+	
+int main() {
+    myclass ob(120);
+    myclass *p;
+    p=&ob;
+    cout << "Value using object: " << ob.get() <<endl;
+    cout << "Value using pointer: " << p->get() <<endl;
+	return 0;
+}
