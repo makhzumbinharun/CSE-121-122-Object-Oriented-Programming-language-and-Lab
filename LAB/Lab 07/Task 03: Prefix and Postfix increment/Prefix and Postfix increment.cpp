3859c332670741f6a0f7d7459f23fac24d76302a
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-03-31 14:27:45 +0600

    Implement prefix and postfix increment operators

--- LAB/Lab 07/Task 03: Prefix and Postfix increment/Prefix and Postfix increment.cpp
+++ LAB/Lab 07/Task 03: Prefix and Postfix increment/Prefix and Postfix increment.cpp
@@ -1,1 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+class Distance{
+    private:
+    int meters;
+    
+    public:
+
+    Distance(int x=0){
+        meters=x;
+    }
+    
+    Distance operator++(){
+        ++meters;
+        return *this;
+    }
+    Distance operator++(int){
+        Distance temp=*this;
+        meters++;
+        return temp;
+    }
+    
+    void display(){
+        cout << meters<<endl;
+    }
+};
+
+
+
+int main() {
+	Distance d1(2);
+	cout << "Inital: ";
+	d1.display();
+	++d1;
+	cout << "Prefix Increment: ";
+	d1.display();
+	cout << "Prefix Decrement: ";
+	d1++;
+	d1.display();
+	return 0;
+}
