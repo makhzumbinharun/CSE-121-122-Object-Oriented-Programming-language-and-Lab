7e2fdae8b8ab4cca326782c3524862c3a4f987b6
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-04-07 12:51:51 +0600

    Implement operator overloading for Student class

---
+++ LAB/Lab 08/Operator Overloading Less Than and Not Equal/Operator Overloading Less Than and Not Equal.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Student{
+    int English, Bangla, Math;
+public:
+    Student(){
+        English=0,Bangla=0,Math=0;
+    }
+    
+    Student(int i, int j, int k){
+        English=i;
+        Bangla=j;
+        Math=k;
+    }
+    
+    int operator<(Student ob){
+        int avg1=(English+Bangla+Math)/3;
+        int avg2=(ob.English+ob.Bangla+ob.Math)/3;
+    return (avg1<avg2);
+}
+    int operator!=(Student ob){
+        int avg1=(English+Bangla+Math)/3;
+        int avg2=(ob.English+ob.Bangla+ob.Math)/3;
+    return (avg1!=avg2);
+}
+
+    
+    void display(){
+        cout << "English: " << English << " Bangla: " << Bangla << " Math: " << Math << endl;
+    }
+};
+
+int main() {
+	Student MBH(10, 20, 30), MRI(40, 50, 60);
+	MBH.display();
+	MRI.display();
+	
+	cout << "\nLess Than Overloading\n";
+	if(MBH<MRI) cout << "MRI is the topper";
+	else cout << "MBH is the topper";
+	
+	cout << "\nNot Equal Overloading\n" << endl;
+	if(MBH!=MRI) cout << "Not Equal";
+	else cout << "Equal";
+	
+return 0;
+}
+
+
+
+
