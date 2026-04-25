d0bc313a34909ab035b74114349d95d0bf7c7f6c
Makhzum-Bin-Harun <76556113+makhzumbinharun@users.noreply.github.com>
2026-04-25 11:40:23 +0600

    Add Student class with comparison operators

---
+++ FINAL PRACTICE/SECTION 08/Question 02.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student {
+private:
+    int id;
+    int age;
+    string name;
+public:
+    Student(int i, int a, string n) {
+        id = i;
+        age = a;
+        name = n;
+    }
+
+    friend bool operator<(const Student &ob1, const Student &ob2);
+
+    friend bool operator!=(const Student &ob1, const Student &ob2);
+};
+
+    bool operator<(const Student &ob1, const Student &ob2){
+        return ob1.age < ob2.age;
+    }
+
+    bool operator!=(const Student &ob1, const Student &ob2){
+        return ob1.age != ob2.age;
+    }
+
+int main() {
+    Student H1(272, 24, "EAT"), H2(279, 20, "MBH");
+
+    if (H1 < H2)
+        cout << "Second one is elder!" << endl;
+    else
+        cout << "First one is elder!" << endl;
+
+    if (H1 != H2)
+        cout << "They are not the same age" << endl;
+
+    return 0;
+}
